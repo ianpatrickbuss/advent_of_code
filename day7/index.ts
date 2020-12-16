cb7df9d749f7e93421f0a7ab5fb31c75433cf9dc
Ian Buss <ian@starcrusher.productions>
2020-12-16 09:28:06 -0500

    Day 7 part 2 solved

--- day7/index.ts
+++ day7/index.ts
@@ -1,65 +1,64 @@
-const bagMap: Map<string, string[]> = new Map();
+const getEligibleBags = (lines: string[]): Set<string> => {
+  const eligibleBags: Set<string> = new Set();
 
-const bagRegX = /(\d)|(bags)|(bag)|(\.)/g
+  const getHead = (line: string): string => line.split('contain')[0].replace(/(bags)|(bag)/,'').trim();
+  
+  const findHeads = (head: string): string[] => lines.filter(rule => {
+    const contain = new RegExp(`(${head})`);
+    const startWith = new RegExp(`^(${head})`);
+    return contain.test(rule) && !startWith.test(rule)
+  }).map(rule => getHead(rule));
 
-const graphIt = (line: string): void => {
-  const graph = line.split('contain');
-  const head = graph[0].replace(bagRegX,'').trim();
-  if(graph[1].includes('no other')) {
-    return;
-  }
-  const nodes = graph.slice(1).map(node => {
-    return node.split(',').map(bag => {
-      return bag.replace(bagRegX,'').trim()
-    })
-  }).flat();
-  bagMap.set(head,nodes);
+  lines.forEach(line=> {
+    if(line.includes('shiny gold') && !/^(shiny gold)/.test(line)) {
+      const head = getHead(line);
+      eligibleBags.add(head);
+      findHeads(head).forEach(bag => [
+        eligibleBags.add(bag)
+      ]);
+    }
+  })
+
+  eligibleBags.forEach(bag => {
+    findHeads(bag).forEach(b => {
+      eligibleBags.add(b)
+    });
+  })
+  
+  return eligibleBags;
 }
 
-const eligibleBags = new Set();
+const getNumberOfNestedBags = (term: string,lines: string[]): number => {
+  var res = 0;
+  for(let L = 0; L<lines.length; L++) {
+    const line = lines[L];
+    const regex = new RegExp(`^(${term})`);
+    if(regex.test(line) && /\d+/.test(line)) {
+      const numbers = line.match(/\d+/g);
+      if(!numbers) break; 
+      const sum = numbers.map(num => Number.parseInt(num,10)).reduce((L,R) => L+R)
+      res += sum;
+      line.split('contain')[1].split(',').forEach(bag => {
+        const head = bag.replace(/(bags)|(bag)|\d+|\./g,'').trim();
+        const digits = /\d+/.exec(bag);
+        let multipler = 1;
+        if(digits) {
+          multipler = Number.parseInt(digits[0],10);
+        }
+        res += multipler*getNumberOfNestedBags(head,lines);
+      })
+    }
+  }
+  return res;
+}
 
 const main = async () => {
-
   const text = await Deno.readTextFile("./day7/input.txt");
   const lines = text.split('\n');
-  const bagsWithGold: string[] = [];
-  const bagsWithoutGold: string[] = [];
-  lines.forEach(line => {
-    if(line.includes('shiny gold')){
-      bagsWithGold.push(line);
-    } else {
-      bagsWithoutGold.push(line)
-    }
-  })
-  const rules = [...bagsWithGold,...bagsWithoutGold];
-  rules.forEach(graphIt)
-  bagMap.forEach((bags: string[],head: string) => {
-    let eligible = false;
-    for(let i = 0; i<bags.length; i++) {
-      const bag = bags[i];
-      if(eligibleBags.has(bag)) {
-        eligible = true;
-        break
-      }
-    }
-    if(bags.includes('shiny gold') || eligible){
-      eligibleBags.add(head);
-    }
-  })
-
-  
-  console.log(eligibleBags.size)  
-  // console.group('bagMap')
-  // bagMap.forEach((bag,head) => {
-  //   console.log(`${head} => ${bag}`);
-  // })
-  // console.groupEnd();
-  // console.log('-------');
-  // console.group('bags:')
-  // eligibleBags.forEach((val) => {
-  //   console.log(val)
-  // })
-  // console.groupEnd()
+  const part1 = getEligibleBags(lines); 
+  console.log(part1)
+  const part2 = getNumberOfNestedBags('shiny gold',lines);
+  console.log(part2)
 }
 
 main()
